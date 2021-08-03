4026608b3c096bf2a19753c0e816852f334fb8f0
Bradley Bottomlee <bradley.bttm@gmail.com>
2021-08-03 10:21:37 -0400

    Fix sample OCR script to omit empty lines

--- ocr_example.py
+++ ocr_example.py
@@ -6,5 +6,10 @@ if __name__ == "__main__":
     sample = pytesseract.image_to_string(Image.open("./split-images/0001/Entity1.jpg"))
 
     sampleFile = open("0001_ocr_sample.txt", "w")
-    sampleFile.write(sample)
+
+    for line in sample.split("\n"):
+        if not line.strip():
+            continue 
+        sampleFile.write(line + "\n")
+
     sampleFile.close()
